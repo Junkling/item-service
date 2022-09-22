ede65f0272d47bc16c35aab3dbe6f4a94e08df31
Junkling <ddoly0106@gmail.com>
2022-09-22 18:07:27 +0900

    Initial commit

---
+++ src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,65 @@
+package hello.itemservice.domain.item;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach(){
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Item item = new Item("itemA", 10000, 10);
+        //when
+        Item saveItem = itemRepository.save(item);
+        //then
+        Item byId = itemRepository.findById(item.getId());
+        assertThat(byId).isEqualTo(saveItem);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Item itemA = new Item("itemA", 10000, 10);
+        Item itemB = new Item("itemB", 20000, 20);
+        itemRepository.save(itemA);
+        itemRepository.save(itemB);
+        //when
+        List<Item> result = itemRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(itemA, itemB);
+    }
+
+    @Test
+    void updateItem() {
+        //given
+        Item itemA = new Item("itemA", 10000, 10);
+        Item savedId = itemRepository.save(itemA);
+        Long itemId = savedId.getId();
+        //when
+        Item updateItem = new Item("itemB", 20000, 20);
+        itemRepository.update(itemId, updateItem);
+
+        //then
+        Item findItem = itemRepository.findById(itemId);
+        assertThat(findItem.getItemName()).isEqualTo(updateItem.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateItem.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateItem.getQuantity());
+
+    }
+}
